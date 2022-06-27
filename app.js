290d7b3c8200f7b4d7ef5874e51d205790fe78ba
Henry Chang <humiya.chang@hotmail.com>
2022-06-27 15:38:54 +0000

    Change: Create a swagger document for your APIs.

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 require('dotenv').config();
 
 const express = require('express');
+const swaggerUi = require('swagger-ui-express');
 const route = require('./src/router');
 const apiResp = require('./src/helper/api-response');
+const swaggerDocument = require('./swagger.json');
 
 function startSever() {
   const app = express();
   const port = process.env.PORT || 3000;
+  app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
   app.use(express.json());
   app.use('/api/v1/', route);
   /* eslint-disable-next-line no-unused-vars */
